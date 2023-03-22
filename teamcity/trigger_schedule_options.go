5178feb4f02485857d678e777530be961c99acf4
Yi Zhang <yzhang@yext.com>
2023-03-22 16:49:34 -0400

    Support BuildTriggerCustomization
    enforceCleanCheckout and enforceCleanCheckoutForDependencies are no longer
    in the property field, moved them to BuildTriggerCustomization according
    to the API change

--- teamcity/trigger_schedule_options.go
+++ teamcity/trigger_schedule_options.go
@@ -2,16 +2,16 @@ package teamcity
 
 //TriggerScheduleOptions represent options for configuring a scheduled build trigger
 type TriggerScheduleOptions struct {
-	QueueOptimization                   bool                       `prop:"enableQueueOptimization" force:""`
-	PromoteWatchedBuild                 bool                       `prop:"promoteWatchedBuild" force:""`
-	BuildWithPendingChangesOnly         bool                       `prop:"triggerBuildWithPendingChangesOnly" force:""`
-	TriggerIfWatchedBuildChanges        bool                       `prop:"triggerBuildIfWatchedBuildChanges"`
-	BuildOnAllCompatibleAgents          bool                       `prop:"triggerBuildOnAllCompatibleAgents"`
-	RevisionRuleSourceBuildID           string                     `prop:"revisionRuleDependsOn"`
-	RevisionRule                        ArtifactDependencyRevision `prop:"revisionRule"`
-	RevisionRuleBuildBranch             string                     `prop:"revisionRuleBuildBranch"`
-	EnforceCleanCheckout                bool                       `prop:"enforceCleanCheckout"`
-	EnforceCleanCheckoutForDependencies bool                       `prop:"enforceCleanCheckoutForDependencies"`
+	QueueOptimization            bool                       `prop:"enableQueueOptimization" force:""`
+	PromoteWatchedBuild          bool                       `prop:"promoteWatchedBuild" force:""`
+	BuildWithPendingChangesOnly  bool                       `prop:"triggerBuildWithPendingChangesOnly" force:""`
+	TriggerIfWatchedBuildChanges bool                       `prop:"triggerBuildIfWatchedBuildChanges"`
+	BuildOnAllCompatibleAgents   bool                       `prop:"triggerBuildOnAllCompatibleAgents"`
+	RevisionRuleSourceBuildID    string                     `prop:"revisionRuleDependsOn"`
+	RevisionRule                 ArtifactDependencyRevision `prop:"revisionRule"`
+	RevisionRuleBuildBranch      string                     `prop:"revisionRuleBuildBranch"`
+
+	BuildCustomization *BuildTriggerCustomization
 }
 
 //NewTriggerScheduleOptions returns a TriggerScheduleOptions with default values
